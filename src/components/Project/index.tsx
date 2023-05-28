f28255503ee0f19b22608a814d265f115755a921
tomazcx <tomazcx06@gmail.com>
2023-05-28 18:43:07 -0300

    fix: fixed projects dimensions

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -8,7 +8,7 @@ type Project= {
 export const Project:React.FC<Project> = ({title, description,...rest}) => {
 
     return (
-        <div className="w-full h-full overflow-hidden group rounded lg border-2 border-white relative cursor-pointer lg:my-0">
+        <div className="lg:h-full w-full overflow-hidden group rounded-lg border-2 border-white relative cursor-pointer  h-[200px] ">
             <img className="h-full w-full object-cover transition-all place-content-end cursor-pointer group-hover:scale-[1.1] group-hover:brightness-[0.3]" {...rest} />
             <div className="flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:flex items-center gap-4">
                 <h1 className="text-lg lg:text-xl font-semibold text-center select-none">{title}</h1>
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -14,18 +14,18 @@ export const ProjectsSection = () => {
     return (
         <section id="projects" className="relative min-h-screen lg:min-h-full flex flex-col justify-center gap-12 my-[20vh] lg:my-0 ">
             <Title text={t("projectsSection.title")} />
-            <div className="lg:grid flex flex-col grid-cols-12 w-11/12 items-center mx-auto gap-8 lg:gap-12">
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.3, ease:"easeOut"}} className="col-span-4 lg:h-[300px]">
+            <div className="lg:grid flex flex-col grid-cols-12 w-11/12 items-center mx-auto  gap-8 lg:gap-12">
+                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.3, ease:"easeOut"}} className="col-span-4 w-11/12 lg:w-full mx-auto lg:mx-0 lg:h-[300px]">
                     <Project title="Crwn Clothing" description={t("projectsSection.crwn-clothing.description")} src={CrwnClothing} alt="Crwn Clothing project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.6, ease:"easeOut"}} className="col-span-8 lg:h-[300px]">
+                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.6, ease:"easeOut"}} className="col-span-8 w-11/12 lg:w-full mx-auto lg:h-[300px]">
                     <Project src={GlassOn} title="Glass On" description={t("projectsSection.glass-on.description")} alt="Glass On project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.9, ease:"easeOut"}} className="col-span-7 lg:h-[300px]">
+                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.9, ease:"easeOut"}} className="col-span-7 w-11/12 lg:w-full mx-auto lg:h-[300px]">
 
                     <Project src={OdontoDashboard} title="Odonto Dashboard" description={t("projectsSection.odonto-dashboard.description")} alt="Odonto Dashboard project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:1.2, ease:"easeOut"}} className="col-span-5 h-[300px]">
+                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:1.2, ease:"easeOut"}} className="col-span-5 w-11/12 lg:w-full mx-auto h-[300px]">
 
                     <Project src={MyPersonalTrainer} title="My Personal Trainer" description={t("projectsSection.my-personal-trainer.description")} alt="My Personal Trainer project" />
                 </motion.div>
