b940aef8d42d865bed65a51a442fc58db7e78166
tomazcx <tomazcx06@gmail.com>
2023-05-28 18:20:10 -0300

    fix: removed image animation for mobile about section

--- src/components/AboutSectionMobile/index.tsx
+++ src/components/AboutSectionMobile/index.tsx
@@ -12,12 +12,12 @@ export const AboutSectionMobile: React.FC = () => {
             <article className='flex flex-col items-center gap-12 w-10/12 max-w-[900px]'>
                 <Title text='Sou um desenvolvedor fullstack' />
                 <div className="flex flex-col items-center gap-8 justify-center relative">
-                    <motion.div initial={{opacity:0}} whileInView={{ opacity:1 }} transition={{ duration: 1, ease: "easeOut", delay: 3 }} className='w-[250px] h-[250px]'>
+                    <div  className='w-[250px] h-[250px]'>
                         <img src={picture} alt='Selfie do desenvolvedor' className='w-full h-full  rounded-full object-cover' />
                         <motion.div initial={{ opacity: 0 }} className='relative' whileInView={{ opacity: 1 }} transition={{ type: 'linear', duration: .2, delay: 6 }}>
                             <motion.img initial={{ rotate: -25 }} animate={{ rotate: 25 }} transition={{ type: "linear", duration: .3, repeatType: 'reverse', repeat: Infinity }} src={WavingHand} alt='Emoji acenando' className='absolute w-[120px] bottom-[-30px] left-[-40px]' />
                         </motion.div>
-                    </motion.div>
+                    </div>
 
                     <div className="flex flex-col gap-4 w-[400px] text-center">
                         <motion.p initial={{ opacity: 0, y: -10 }} className='' whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 4, duration: .5, ease: "linear" }}>
