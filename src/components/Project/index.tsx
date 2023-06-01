ccce80e5d3e5bbecb797c97e72f3c9d546c902aa
tomazcx <tomazcx06@gmail.com>
2023-06-01 11:59:40 -0300

    feat: added links to projects

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -3,18 +3,20 @@ import { ImgHTMLAttributes } from "react"
 type Project= {
     title: string
     description:string
+    href: string
+    target:string
 } & ImgHTMLAttributes<HTMLImageElement>
 
-export const Project:React.FC<Project> = ({title, description,...rest}) => {
+export const Project:React.FC<Project> = ({title, description,href, target,...rest}) => {
 
     return (
-        <div className="lg:h-full w-full overflow-hidden group rounded-lg border-2 border-white relative cursor-pointer  h-[200px] ">
+        <a href={href} target={target} className="lg:h-full w-full overflow-hidden group rounded-lg border-2 border-white relative cursor-pointer block  h-[200px] ">
             <img className="h-full w-full object-cover transition-all place-content-end cursor-pointer group-hover:scale-[1.1] group-hover:brightness-[0.3]" {...rest} />
             <div className="flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:flex items-center gap-4">
                 <h1 className="text-lg lg:text-xl font-semibold text-center select-none">{title}</h1>
                 <p className="text-center text-sm lg:text-md select-none">{description}</p>
             </div>
-        </div>
+        </a>
 
     )
 
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -1,6 +1,6 @@
 import { Project } from "../Project"
 import { Title } from "../Title"
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import GlassOn from '../../assets/projects/glass-on.png'
 import CrwnClothing from '../../assets/projects/crwn-clothing.png'
 import OdontoDashboard from '../../assets/projects/odonto dasbhoard.png'
@@ -9,25 +9,23 @@ import { useTranslation } from "react-i18next"
 
 export const ProjectsSection = () => {
 
-    const {t} = useTranslation()
+    const { t } = useTranslation()
 
     return (
         <section id="projects" className="relative min-h-screen lg:min-h-full flex flex-col justify-center gap-12 my-[20vh] lg:my-0 ">
             <Title text={t("projectsSection.title")} />
             <div className="lg:grid flex flex-col grid-cols-12 w-11/12 items-center mx-auto  gap-8 lg:gap-12">
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.3, ease:"easeOut"}} className="col-span-4 w-11/12 lg:w-full mx-auto lg:mx-0 lg:h-[300px]">
-                    <Project title="Crwn Clothing" description={t("projectsSection.crwn-clothing.description")} src={CrwnClothing} alt="Crwn Clothing project" />
+                <motion.div initial={{ x: -50, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: .6, delay: .3, ease: "easeOut" }} className="col-span-4 w-11/12 lg:w-full mx-auto lg:mx-0 lg:h-[300px]">
+                    <Project href="https://github.com/tomazcx/crwn-clothing" target="_blank" title="Crwn Clothing" description={t("projectsSection.crwn-clothing.description")} src={CrwnClothing} alt="Crwn Clothing project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.6, ease:"easeOut"}} className="col-span-8 w-11/12 lg:w-full mx-auto lg:h-[300px]">
-                    <Project src={GlassOn} title="Glass On" description={t("projectsSection.glass-on.description")} alt="Glass On project" />
+                <motion.div initial={{ x: -50, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: .6, delay: .6, ease: "easeOut" }} className="col-span-8 w-11/12 lg:w-full mx-auto lg:h-[300px]">
+                    <Project href="https://github.com/tomazcx/next-glass-on" target="_blank" src={GlassOn} title="Glass On" description={t("projectsSection.glass-on.description")} alt="Glass On project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:.9, ease:"easeOut"}} className="col-span-7 w-11/12 lg:w-full mx-auto lg:h-[300px]">
-
-                    <Project src={OdontoDashboard} title="Odonto Dashboard" description={t("projectsSection.odonto-dashboard.description")} alt="Odonto Dashboard project" />
+                <motion.div initial={{ x: -50, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: .6, delay: .9, ease: "easeOut" }} className="col-span-7 w-11/12 lg:w-full mx-auto lg:h-[300px]">
+                    <Project href="https://github.com/tomazcx/odonto-dashboard" target="_blank" src={OdontoDashboard} title="Odonto Dashboard" description={t("projectsSection.odonto-dashboard.description")} alt="Odonto Dashboard project" />
                 </motion.div>
-                <motion.div initial={{x:-50,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:.6, delay:1.2, ease:"easeOut"}} className="col-span-5 w-11/12 lg:w-full mx-auto h-[300px]">
-
-                    <Project src={MyPersonalTrainer} title="My Personal Trainer" description={t("projectsSection.my-personal-trainer.description")} alt="My Personal Trainer project" />
+                <motion.div initial={{ x: -50, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: .6, delay: 1.2, ease: "easeOut" }} className="col-span-5 w-11/12 lg:w-full mx-auto h-[300px]">
+                    <Project href="https://github.com/tomazcx/my-personal-trainer-web" target="_blank" src={MyPersonalTrainer} title="My Personal Trainer" description={t("projectsSection.my-personal-trainer.description")} alt="My Personal Trainer project" />
                 </motion.div>
 
             </div>
